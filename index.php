68870b066779fae323ebcad0465d0493ca0c1ad1
riaa44 <riya@thekraftors.com>
2025-03-04 11:57:59 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Simple PHP script to display a message
+
+// Set the content type to HTML
+header('Content-Type: text/html');
+
+// Display a welcome message
+echo "<h1>Welcome to My PHP Page</h1>";
+echo "<p>This is a simple PHP script.</p>";
+
+// Display the current date and time
+echo "<p>Current date and time: " . date('Y-m-d H:i:s') . "</p>";
+?>
