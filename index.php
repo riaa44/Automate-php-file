a20c0873572fe3021466f84a2f31ae616eb9dff8
riaa44 <riya@thekraftors.com>
2025-03-04 14:54:44 +0530

    Updated message

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ header('Content-Type: text/html');
 
 // Display a welcome message
 echo "<h1>Welcome to My PHP Page</h1>";
-echo "<p>This is a simple PHP script.</p>";
+echo "<p>This is a simple PHP script. Development automatically.</p>";
 
 // Display the current date and time
 echo "<p>Current date and time: " . date('Y-m-d H:i:s') . "</p>";
